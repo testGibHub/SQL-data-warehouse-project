041ba2f824b53a180ec23b04bb54106d2b4e0a0b
testGibHub <125273874+testGibHub@users.noreply.github.com>
2026-09-08 15:40:14 +0100

    Update Performance_Analysis.sql

--- scripts/Performance_Analysis.sql
+++ scripts/Performance_Analysis.sql
@@ -1,1 +1,40 @@
+/*Analyze the yearly performance of products 
+by comparing their sales to both the 
+average sales performance of the 
+product and the previous years sales*/
+
+WITH yearly_product_sales AS (
+SELECT
+YEAR(f.order_date) AS order_year,
+p.product_name,
+SUM(f.sales_amount) As current_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+WHERE f.order_date IS NOT NULL AND p.product_name IS NOT NULL
+GROUP BY 
+YEAR(f.order_date),
+p.product_name
+)
+
+SELECT order_year,
+product_name,
+current_sales,
+AVG(current_sales) OVER(PARTITION BY product_name) avg_sales,  
+current_sales-AVG(current_sales) OVER(PARTITION BY product_name) As diff_avg,
+CASE WHEN current_sales-AVG(current_sales) OVER(PARTITION BY product_name) > 0 THEN 'Above average'
+	WHEN current_sales-AVG(current_sales) OVER(PARTITION BY product_name) < 0 THEN 'Below Average'
+	ELSE 'Avg'
+END avg_change,
+--YOY analysis
+LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS py_sales,
+current_sales- LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) As diff_py,
+CASE WHEN current_sales-LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
+	WHEN current_sales-LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
+	ELSE 'No change'
+END py_change
+FROM yearly_product_sales
+ORDER BY product_name, order_year
+
+
 
