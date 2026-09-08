ac893686ffebde8f4d808f6c98ef3fbf7dff8692
testGibHub <125273874+testGibHub@users.noreply.github.com>
2026-09-08 15:43:59 +0100

    Update Date_Exploration.sql

--- scripts/Date_Exploration.sql
+++ scripts/Date_Exploration.sql
@@ -1,1 +1,14 @@
+--Date Exploration
+--Find the date of the first and last order
+--How many years of sales are available
 
+SELECT MIN(order_date) As first_order_Date, 
+MAX(order_date) As last_order_date ,
+DATEDIFF(MONTH,MIN(order_date), MAX(order_date)) AS diff_in_dates
+FROM gold.fact_sales;
+
+--Find the youngest and oldest customer
+SELECT MIN(birthdate)As oldest_customer,
+DATEDIFF(year,MIN(birthdate), GETDATE()) AS oldest_age,
+MAX(birthdate) AS youngest_customer,
+DATEDIFF(year, MAX(birthdate), GETDATE()) AS youngest_date FROM gold.dim_customers ;
