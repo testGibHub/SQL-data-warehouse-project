55dc2854dbfaa2be27e950b264e737ec559cf4a2
testGibHub <125273874+testGibHub@users.noreply.github.com>
2026-09-08 15:43:24 +0100

    Update Exploring_Dimensions.sql

--- scripts/Exploring_Dimensions.sql
+++ scripts/Exploring_Dimensions.sql
@@ -1,1 +1,13 @@
+--Exploring Dimensions
+---Explore all countries our customers come from.
+
+SELECT DISTINCT country
+FROM gold.dim_customers;
+
+--Explore all categories "The major Divisions"
+SELECT DISTINCT category, subcategory, product_name
+FROM gold.dim_products
+ORDER BY 1,2,3;
+
+
 
